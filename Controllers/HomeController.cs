309dda250636afd9a0e071c41276c643e1dc316c
Moses <lazaromoses77@gmail.com>
2020-07-03 17:11:34 +0300

    commit all app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using PeoplesUniversity.Data;
+using PeoplesUniversity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeoplesUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PeoplesUniversityContext db = new PeoplesUniversityContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
